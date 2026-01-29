a5dbe63feb11075678e6144dc870682117839c0b
haflettjm <hafletjm01@gmail.com>
2026-01-29 10:19:44 -0500

    looks like cloudrun is using healthz i forgot

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -36,7 +36,7 @@ type server struct {
 }
 
 func (s server) registerRoutes(r *gin.Engine) {
-	r.GET("/healthz", func(c *gin.Context) {
+	r.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"status": "ok"})
 	})
 
